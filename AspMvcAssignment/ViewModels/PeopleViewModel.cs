d5e68577742428c99f5375861fa5751c5d21a437
Abeerahmedg <112552899+Abeerahmedg@users.noreply.github.com>
2022-11-20 12:20:16 +0100

    Update PeopleViewModel.cs

--- AspMvcAssignment/ViewModels/PeopleViewModel.cs
+++ AspMvcAssignment/ViewModels/PeopleViewModel.cs
@@ -1,13 +1,27 @@
-﻿
+
 using AspMvcAssignment.Models;
 
 namespace AspMvcAssignment.ViewModels
 {
-    public class PeopleViewModel : CreatePersonViewModel
+    public class PeopleViewModel
     {
-        public static List<Person> PeopleList { get; set; } = new List<Person>();
+        public static List<Person> PeopleList = new List<Person>();
        // public string Search { get; set; }
-        public List<Person> tempList { get; set; } = new List<Person>();
+        public List<Person> tempList = new List<Person>();
+
+        public static void FillPeople()
+        {
+            Person p1 = new(Guid.NewGuid().ToString(), "Agatha Christie", 85, "Devon");
+            PeopleViewModel.PeopleList.Add(p1);
+            Person p2 = new(Guid.NewGuid().ToString(), "Dan Brown", 7, "New Hampshire");
+            PeopleViewModel.PeopleList.Add(p2);
+            Person p3 = new(Guid.NewGuid().ToString(), "Yasuo Uchida", 130, "Tokyo");
+            PeopleViewModel.PeopleList.Add(p3);
+            Person p4 = new(Guid.NewGuid().ToString(), "Ahmed Tawfik", 200, "Tanta");
+            PeopleViewModel.PeopleList.Add(p4);
+
+
+        }
 
     }
 }
