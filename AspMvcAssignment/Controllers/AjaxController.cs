cfff04569217da53f364303b127b7a5c05a3ca25
Abeerahmedg <112552899+Abeerahmedg@users.noreply.github.com>
2022-11-20 12:13:32 +0100

    Update AjaxController.cs

--- AspMvcAssignment/Controllers/AjaxController.cs
+++ AspMvcAssignment/Controllers/AjaxController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using AspMvcAssignment.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using AspMvcAssignment.Models;
 
 namespace AspMvcAssignment.Controllers
 {
@@ -6,7 +8,52 @@ namespace AspMvcAssignment.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            PeopleViewModel pvm = new();
+            pvm.tempList = PeopleViewModel.PeopleList;
+            return View(pvm);
+        }
+        [HttpPost]
+        public IActionResult GetDetails(string id)
+        {
+            Person person = PeopleViewModel.PeopleList.FirstOrDefault(i => i.Id == id);
+            if (person == null)
+            {
+                return Json("The writer name was not found!");
+            }
+            return PartialView("_peopleDeatilsPartial", person);
+          /*  PeopleViewModel pvm = new();
+            foreach (Person person in PeopleViewModel.PeopleList)
+            {
+                if (person.Id == id.Trim())
+                {
+                    pvm.tempList.Add(person);
+                    return PartialView("_PeopleDetailsPartial", pvm);
+                }
+            }
+            pvm.tempList = PeopleViewModel.PeopleList;
+            return PartialView("_PeopleDetailsPartial", pvm);*/
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            foreach (Person person in PeopleViewModel.PeopleList)
+            {
+                if (person.Id == id)
+                {
+                    PeopleViewModel.PeopleList.Remove(person);
+                    return Json(id + " was Succefully deleted");
+                }
+            }
+            return Json("Could not delete it!");
+        }
+        [HttpGet]
+        public IActionResult CreatePeople()
+        {
+            PeopleViewModel pvm = new();
+            pvm.tempList = PeopleViewModel.PeopleList;
+   
+            return PartialView("_PeoplePartial",pvm);
         }
     }
 }
